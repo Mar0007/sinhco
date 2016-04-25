c0e8db0fb315d89a3b5106377215814e1fdeaa34
Javier Peña <jprockx8@hotmail.com>
2016-04-25 15:38:26 -0600

    s

--- xdaxshboxard.php
+++ xdaxshboxard.php
@@ -14,6 +14,7 @@
     //$modulo = ( isset($_GET['mod']) ) ? $_GET['mod'] : "dashboard";                                      
     //Comentario en el xdaxshboxard
     //Que comentario mas malo eso javi...
+    //lo hizo automatico muela, no la verdad es que si es malo
     $modulo = ( !URLParam(1) ) ? "dashboard" : URLParam(1);
     
     echo $modulo;
