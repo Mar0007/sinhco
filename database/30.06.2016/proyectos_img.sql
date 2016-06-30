7917ee92d23c85efc13fd03dbb632ff5ee8f77e5
luis lobo <luisd.lobo@hotmail.com>
2016-06-30 17:52:38 -0600

    Tablas Proyectos_img + Modulos

---
+++ database/30.06.2016/proyectos_img.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-07-2016 a las 01:51:26
+-- Versión del servidor: 5.6.15-log
+-- Versión de PHP: 5.5.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `bd5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyectos_img`
+--
+
+CREATE TABLE IF NOT EXISTS `proyectos_img` (
+  `idproyecto` int(11) NOT NULL,
+  `idimagen` int(11) NOT NULL,
+  PRIMARY KEY (`idproyecto`,`idimagen`),
+  KEY `proyectos_img_ibfk_2` (`idimagen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `proyectos_img`
+--
+
+INSERT INTO `proyectos_img` (`idproyecto`, `idimagen`) VALUES
+(78, 196);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `proyectos_img`
+--
+ALTER TABLE `proyectos_img`
+  ADD CONSTRAINT `proyectos_img_ibfk_1` FOREIGN KEY (`idproyecto`) REFERENCES `proyectos` (`idproyecto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `proyectos_img_ibfk_2` FOREIGN KEY (`idimagen`) REFERENCES `imagenes` (`idimagen`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
